659e69ea1e5c738cb43e28462922148e654cb89a
jenis navadiya <jenisnavadiya04@gmail.com>
2023-03-20 12:10:28 +0530

    code

--- Get Api/ViewController.swift
+++ Get Api/ViewController.swift
@@ -1,19 +1,59 @@
-//
-//  ViewController.swift
-//  Get Api
-//
-//  Created by Jenish Navadiya on 17/03/23.
-//
 
 import UIKit
+struct GetApi : Codable{
 
-class ViewController: UIViewController {
+    var postId : Int
+    var id : Int
+    var name : String
+    var email : String
+    var body : String
+}
 
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
+  var arr : [GetApi] = []
+
+ 
+    @IBOutlet weak var dataTabel: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getData()
+ 
     }
-
-
+    func getData(){
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1/comments")
+        var ur = URLRequest(url: url!)
+        ur.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: ur) { data, response, error in
+            do{
+                if error == nil{
+                    self.arr = try JSONDecoder().decode([GetApi].self, from: data!)
+                    print(self.arr)
+                    DispatchQueue.main.async {
+                        self.dataTabel.reloadData()
+                    }
+                }
+            }
+                catch{
+                    print(error.localizedDescription)
+                }
+            }.resume()
+   }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = dataTabel.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyTableViewCell
+        cell.label1.text = "\(arr[indexPath.row].id)"
+        cell.label2.text = arr[indexPath.row].email
+        return cell 
+        
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
 }
 
