6092bf7830f24adb1797fb06b4a13be4537c7964
jenis navadiya <jenisnavadiya04@gmail.com>
2023-03-22 12:04:51 +0530

    1

--- Get Api/ViewController.swift
+++ Get Api/ViewController.swift
@@ -1,7 +1,9 @@
 
 import UIKit
-struct GetApi : Codable{
+import Alamofire
 
+struct GetApi : Codable{
+    
     var postId : Int
     var id : Int
     var name : String
@@ -11,49 +13,71 @@ struct GetApi : Codable{
 
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-   
-  var arr : [GetApi] = []
-
- 
+    
+    var arr : [GetApi] = []
+    
+    
     @IBOutlet weak var dataTabel: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         getData()
- 
-    }
-    func getData(){
-        let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1/comments")
-        var ur = URLRequest(url: url!)
-        ur.httpMethod = "GET"
         
-        URLSession.shared.dataTask(with: ur) { data, response, error in
-            do{
-                if error == nil{
-                    self.arr = try JSONDecoder().decode([GetApi].self, from: data!)
-                    print(self.arr)
-                    DispatchQueue.main.async {
-                        self.dataTabel.reloadData()
+    }
+    //    func getData(){
+    //        let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1/comments")
+    //        var ur = URLRequest(url: url!)
+    //        ur.httpMethod = "GET"
+    //
+    //        URLSession.shared.dataTask(with: ur) { data, response, error in
+    //            do{
+    //                if error == nil{
+    //                    self.arr = try JSONDecoder().decode([GetApi].self, from: data!)
+    //                    print(self.arr)
+    //                    DispatchQueue.main.async {
+    //                        self.dataTabel.reloadData()
+    //                    }
+    //                }
+    //            }
+    //                catch{
+    //                    print(error.localizedDescription)
+    //                }
+    //            }.resume()
+    //   }
+    
+    func getData(){
+        AF.request("https://jsonplaceholder.typicode.com/posts/1/comments",method: .get).responseData { respo in
+            switch respo.result{
+            case.success(let data):
+                do{
+                    if data == respo.value{
+                        self.arr = try JSONDecoder().decode([GetApi].self, from: data)
+                        print(self.arr)
+                        DispatchQueue.main.async {
+                            self.dataTabel.reloadData()
+                        }
                     }
                 }
-            }
                 catch{
                     print(error.localizedDescription)
                 }
-            }.resume()
-   }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arr.count
+            case.failure(let err):
+                print(err.localizedDescription)
+            }
+        }
     }
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return arr.count
+}
+
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = dataTabel.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyTableViewCell
+    cell.label1.text = "\(arr[indexPath.row].id)"
+    cell.label2.text = arr[indexPath.row].email
+    return cell
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = dataTabel.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyTableViewCell
-        cell.label1.text = "\(arr[indexPath.row].id)"
-        cell.label2.text = arr[indexPath.row].email
-        return cell 
-        
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150
-    }
+}
+func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 150
+}
 }
 
